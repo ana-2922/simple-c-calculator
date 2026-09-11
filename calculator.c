74aa535e95ae8adcdb17993318624d4bfc84c0a3
ana-2922 <analise.labriola@siu.edu>
2026-09-10 19:41:26 -0500

    Implemented multiplication feature

--- calculator.c
+++ calculator.c
@@ -9,14 +9,21 @@ int sub(int a, int b){
 	return a - b;
 }
 
+int mult(int a, int b){
+	return a * b;
+}
+
 
 int main(){
 	int addition;
 	int substraction;
+	int multiplication;
 	addition = add(1, 2);
 	subtraction = sub(3, 4);
+	multiplication = mult(5,6);
 	printf(" 1+2= %d\n", addition);
 	printf(" 3-4= %d\n", subtraction);
+	printf(" 5*6= %d\n", multiplication);
 	return 0;
 
 }
