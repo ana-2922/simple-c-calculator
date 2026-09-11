c697067eddedbb85ac51c27cd8253e50c25e62be
ana-2922 <analise.labriola@siu.edu>
2026-09-10 19:38:25 -0500

    Implemented subtraction feature

--- calculator.c
+++ calculator.c
@@ -5,11 +5,18 @@ int add(int a, int b){
 	return a + b;
 }
 
+int sub(int a, int b){
+	return a - b;
+}
+
 
 int main(){
 	int addition;
+	int substraction;
 	addition = add(1, 2);
+	subtraction = sub(3, 4);
 	printf(" 1+2= %d\n", addition);
+	printf(" 3-4= %d\n", subtraction);
 	return 0;
 
 }
