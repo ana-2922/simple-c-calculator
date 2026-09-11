f270d7490bf92cc9127e7021c46096848c969540
ana-2922 <analise.labriola@siu.edu>
2026-09-10 19:32:02 -0500

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 
+int add(int a, int b){
+	return a + b;
+}
+
 
 int main(){
+	int addition;
+	addition = add(1, 2);
+	printf(" 1+2= %d\n", addition);
 	return 0;
-
-
-
-
 
 }
