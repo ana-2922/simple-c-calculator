fc7abd9c29ef5eef4f48e54e189ee014034029bb
ana-2922 <analise.labriola@siu.edu>
2026-09-10 19:44:50 -0500

    Implemented division function

--- calculator.c
+++ calculator.c
@@ -13,17 +13,23 @@ int mult(int a, int b){
 	return a * b;
 }
 
+int div(int a, int b){
+	return a / b;
+}
 
 int main(){
 	int addition;
 	int substraction;
 	int multiplication;
+	int division;
 	addition = add(1, 2);
 	subtraction = sub(3, 4);
-	multiplication = mult(5,6);
+	multiplication = mult(5, 6);
+	division = div(7, 8);
 	printf(" 1+2= %d\n", addition);
 	printf(" 3-4= %d\n", subtraction);
 	printf(" 5*6= %d\n", multiplication);
+	printf(" 7/8= %d\n", division);
 	return 0;
 
 }
